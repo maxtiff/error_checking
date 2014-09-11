98866472ac6780461afd81c097cf1b3fed3dc9e3
Travis <travismay@gmail.com>
2014-09-11 13:32:24 -0500

    Create anonymous function for magnitude

--- test_script.R
+++ test_script.R
@@ -1,8 +1,13 @@
 library(jsonlite)
+library(forecast)
+
+log10_ceiling <- function(x) {
+  10^(ceiling(log10(x)))
+}
 
 base_url <- "http://api.stlouisfed.org/fred/series/observations?series_id="
 
-series_id <- "DGORDER"
+series_id <- "ECOMNSA"
 
 url_api <- "&api_key=76bb1186e704598b725af0a27159fdfc"
 
@@ -10,17 +15,25 @@ type <- "&file_type=json"
 
 units <- "&units=pc1"
 
-full_url <- paste(base_url,series_id,units, url_api, type, sep="")
+full_url <- paste(base_url,series_id, units, url_api, type, sep="")
 
 doc <- fromJSON(full_url)
 
 assign(series_id, data.frame(doc$observations))
 
 drops <- c("realtime_start","realtime_end")
-DGORDER <- DGORDER[,!(names(DGORDER) %in% drops)]
-DGORDER$date <- strptime(DGORDER$date, format="%Y-%m-%d")
+ECOMNSA <- ECOMNSA[,!(names(ECOMNSA) %in% drops)]
+ECOMNSA$date <- strptime(ECOMNSA$date, format="%Y-%m-%d")
 
-good <- complete.cases(DGORDER)
-DGORDER <- DGORDER[good,]
+ECOMNSA$value <- ECOMNSA$value[ECOMNSA$value <= 0 ] <- NA
+# 
+# good <- complete.cases(ECOMNSA)
+# ECOMNSA <- ECOMNSA[good,]
+
+plot(ECOMNSA,type = 'l' )
+
+mag <- lapply(ECOMNSA$value, function(x) 10^(ceiling(log10(x))))
+
+ECOMNSA$mag <- mag
 
       
