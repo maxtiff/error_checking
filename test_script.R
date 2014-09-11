46a4b72592bbfc279d7e697385c8e7b3dec45db8
Travis <travismay@gmail.com>
2014-09-11 09:30:40 -0500

    Attempt forecast with pct chg

--- test_script.R
+++ test_script.R
@@ -8,9 +8,19 @@ url_api <- "&api_key=76bb1186e704598b725af0a27159fdfc"
 
 type <- "&file_type=json"
 
-full_url <- paste(base_url,series_id,url_api, type, sep="")
+units <- "&units=pc1"
+
+full_url <- paste(base_url,series_id,units, url_api, type, sep="")
 
 doc <- fromJSON(full_url)
 
 assign(series_id, data.frame(doc$observations))
 
+drops <- c("realtime_start","realtime_end")
+DGORDER <- DGORDER[,!(names(DGORDER) %in% drops)]
+DGORDER$date <- strptime(DGORDER$date, format="%Y-%m-%d")
+
+good <- complete.cases(DGORDER)
+DGORDER <- DGORDER[good,]
+
+      
