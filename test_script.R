6bea1273d041769aec8d611ea4835001fdc2528a
Travis <travismay@gmail.com>
2014-09-15 16:59:49 -0500

    Calculate sd of percentage change for series

--- test_script.R
+++ test_script.R
@@ -6,6 +6,10 @@ library(forecast)
 #   10^(ceiling(log10(x)))
 # }
 
+get_vintages <- function(series_id) {
+  
+}
+
 base_url <- "http://api.stlouisfed.org/fred/series/observations?series_id="
 
 series_id <- "ECOMNSA"
@@ -14,11 +18,23 @@ url_api <- "&api_key=76bb1186e704598b725af0a27159fdfc"
 
 type <- "&file_type=json"
 
-# units <- "&units=pc1"
+units <- "&units=pc1"
 
-vintage_date <- 
+vintage_date <-  
+  
+vintage <- TRUE
 
-full_url <- paste(base_url,series_id, url_api, type, sep="")
+
+if (vintage == TRUE) {
+  
+  full_url <- paste(base_url,series_id, url_api, units, type, sep="")
+  
+}
+
+else 
+
+{
+  full_url <- paste(base_url,series_id, url_api, type, sep="")
 
 doc <- fromJSON(full_url)
 
@@ -27,8 +43,7 @@ assign(series_id, data.frame(doc$observations))
 drops <- c("realtime_start","realtime_end")
 ECOMNSA <- ECOMNSA[,!(names(ECOMNSA) %in% drops)]
 ECOMNSA$date <- strptime(ECOMNSA$date, format="%Y-%m-%d")
-
-# ECOMNSA$value <- ECOMNSA$value[ECOMNSA$value <= 0 ] <- NA
+ECOMNSA$value <- as.numeric(ECOMNSA$value)
 # 
 # good <- complete.cases(ECOMNSA)
 # ECOMNSA <- ECOMNSA[good,]
@@ -39,6 +54,11 @@ mag <- lapply(as.numeric(ECOMNSA$value), function(x) 10^(floor(log10(x))))
 
 ECOMNSA$mag <- mag
 
-
-
-      
+good <- complete.cases(ECOMNSA$value)
+ECOMNSA <- ECOMNSA[good,]
+mean <- mean(ECOMNSA$value)
+diff <- lapply(ECOMNSA$value, function(x) x - mean)
+sqr <- lapply(diff, function(x) x*x)
+sqr_sum <- sum(as.numeric(sqr))
+dvd_sum_sqrs <- sqr_sum/as.numeric(length(sqr))
+sd_pct_chg <- sqrt(dvd_sum_sqrs)
