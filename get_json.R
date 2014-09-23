90cebc99b29afcfbd6f17bd0cf151ea61c5b46bb
Travis <travismay@gmail.com>
2014-09-23 09:58:24 -0500

    Functional json loader

--- get_json.R
+++ get_json.R
@@ -1,9 +1,25 @@
 library(jsonlite)
 
-getJSON <- function(url, ...) {
-  fromJSON(url, ...)
-}
 
 urls <- c("http://api.stlouisfed.org/fred/series/observations?series_id=ECOMNSA&api_key=76bb1186e704598b725af0a27159fdfc&file_type=json","http://api.stlouisfed.org/fred/series/observations?series_id=DGORDER&api_key=76bb1186e704598b725af0a27159fdfc&file_type=json")
 
 lapply(urls, getJSON())
+
+url <- "http://api.stlouisfed.org/fred/series/observations?series_id=ECOMNSA&api_key=76bb1186e704598b725af0a27159fdfc&file_type=json"
+
+download_file <- function(url, ...) {
+  download.file(url, basename(url),...)  
+}
+lapply(urls, download_file)
+
+getJSON <- function(x, ...) {
+  assign(basename(as.name(x), x))
+  x <- fromJSON(x)
+}
+
+lapply(urls, getJSON)
+
+lapply(urls, function(x) assign(basename(as.name(x)), x))
+       
+lapply(urls, function(x) assign(basename(x), x))
+sapply(urls, getJSON)
