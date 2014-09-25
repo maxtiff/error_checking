3354dc490bb194327b669dce14f2b7ce1655e8d5
Travis <travismay@gmail.com>
2014-09-25 10:11:12 -0500

    Create magnitude and loader scripts

---
+++ magnitude.R
@@ -0,0 +1,28 @@
+get_magnitude <- function(data) {
+  
+  if (units == grep('pct1',units)) {
+    ## Calculate SD of percent change
+    
+    # Find mean
+    mean <- mean(data$value)
+    
+    # Find distance from mean for each observation via anonymous function
+    diff <- lapply(data$value, function(x) x - mean)
+    
+    # Square the distance to get rid of negative values
+    sqr <- lapply(diff, function(x) x*x)
+    
+    # Sum the values of all squared observations
+    sqr_sum <- sum(as.numeric(sqr))
+    
+    # Find divided sum of squares
+    dvd_sum_sqrs <- sqr_sum/(as.numeric(length(sqr)-1))
+    
+    # Take square root of divided sum of squares to get SD.
+    sd_pct_chg <- sqrt(dvd_sum_sqrs)
+  }  
+}
+
+units_level <- function(){}
+
+rate_unites <- function(){}
---
+++ test_loader.R
@@ -0,0 +1,44 @@
+#### Create data frames
+getJSON <- function(id, units, vint) {
+
+  series_id <- id
+  unit <- units
+  vintage <- vint
+  
+  # Establish components of URL for API call
+  base_url <- "http://api.stlouisfed.org/fred/series/observations?series_id="
+  url_api <- "&api_key=76bb1186e704598b725af0a27159fdfc"
+  type <- "&file_type=json"
+  
+  
+  full_url <- paste(base_url,series_id, url_api, units, type, sep="")
+  
+  # Pull JSON from API
+  object <- fromJSON(full_url)
+  
+  # Convert JSON object to data frame for processing
+  data <- data.frame(doc$observations)
+  
+  # Drop realtime start and end
+  drops <- c("realtime_start","realtime_end")
+  data <- data[,!(names(data) %in% drops)]
+  
+  # Convert observation date to appropriate format for time-series analysis
+  data$date <- strptime(data$date, format="%Y-%m-%d")
+  
+  # Convert
+  data$value <- as.numeric(data$value)
+  
+  # Drop NA values.
+  data <- na.omit(data)
+  
+  return(data)
+  
+  ###########################################################################################
+  ##### The data frame should be immutable at this point! Do not attempt to change it! ######
+  ###########################################################################################
+}
+
+get_stats(x) {
+  # pulls information about series from downloaded JSON object
+}
