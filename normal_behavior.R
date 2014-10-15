7094b8d35ce0dc5ba29b5b407c01d01e0b6f2af7
Travis <travismay@gmail.com>
2014-10-15 16:24:16 -0500

    test filter models

--- normal_behavior.R
+++ normal_behavior.R
@@ -5,35 +5,13 @@ determine.freq <- function(object) {
 
 }
 
-create.windows <- function(data, frequency) {
-  ## Needs to be rebuilt to check around new observations and revisions, as opposed to the entire time series as it now stands.
-  ln <- length(data$value)
-  freq <- frequency
-
-  if (freq == 'M') {
-
-    interval <- ceiling(ln/12)
-
-    return(rollapply(data$value,width=interval,diff(quantile(data$value,)),by=interval))
-
-  } else if (is.null(data)) {
-
-    return('fail')
-
-  } else {
-
-    return('something wrong')
-
-  }
-
-}
 
 detect.outliers <- function(data,plot=TRUE) {
 
   ## Read in data.
   x <- as.ts(data)
 
-  ## Estimate trend and components.
+  ## Estimate trend and components and take residual.
   if(frequency(x)>1)
     resid <- stl(x,s.window="periodic",robust=TRUE)$time.series[,3]
   else
@@ -43,11 +21,11 @@ detect.outliers <- function(data,plot=TRUE) {
   }
 
   ## Break into quantiles for outlier detection and score observations on severity.
-  resid.q <- quantile(resid,prob=c(0.2,0.8))
+  resid.q <- quantile(resid,prob=c(0.25,0.75))
 
   ## Establish interquantile range
   iqr <- diff(resid.q)
-  limits <- resid.q + 2*iqr*c(-1,1)
+  limits <- resid.q + 2.5*iqr*c(-1,1)
 
   ## Determine score of data point. Only examine scores above 1.
   score <- floor(abs(pmin((resid-limits[1])/iqr,0) + pmax((resid - limits[2])/iqr,0)))
@@ -89,6 +67,77 @@ check.revisions <- function(vintage,update) {
     print("Error. Exiting")
     exit()
   }
+
 }
 
+observation.windows <- function(data) {
+  ### Create non-overlapping windows.
+  ## Count how many obs total in the time series, as well as handle the quantity of obs inspected by the algorithim and obs that remain to be processed.
+  series.length <- length(data)
+  remaining.obs <- series.length
+  obs.inspected <- 0
+
+  ### Establish number of windows over span of time series.
+  ## 10 is chosen to prevent degrees of freedom errors. This may need adjustment with any appropriate evidence that becomes available as time proceeds.
+  ## Window size is 9 in order to slide the window along the time series without double counting observations.
+  window.size <- 9
+  slider <- 10
+  complete.windows <- floor(series.length/window.size)
+  remainder <- series.length%%10
+  print(paste("Observations remaining after last complete window:",remainder,sep=" "))
+
+  ## Counter variables for window coordinates in list (this is bad practice).
+  n <- 1
+
+  ## Check if new data is identical to previous vintage up to the last observation.
+# if (check.revisions == TRUE) {
+#
+#   score <- detect.outliers(tail(data, n = 10)
+#
+# } else {
+
+   ## Sequence along windows of time series.
+   # Adjust to prevent subscript out of bounds.
+    for (i in seq_along(data)) {
+
+      print(i)
+
+# if (remaining.obs > remainder) {
+      score <- detect.outliers(data[n:(n+window.size),])
+      print(paste("Remaining Observations:",remaining.obs, sep=" "))
+      print(data[n:(n+window.size),])
+      print(score)
+
+      n <- n + slider
+      obs.inspected <- obs.inspected + 10
+      print(paste("Observations inspected:",obs.inspected,sep=" "))
+      remaining.obs <- series.length - obs.inspected
+      print(paste("Remaining Observations:",remaining.obs,sep=" "))
+#     } else if (remaining.obs - window.size == remainder) {
+#        score <- detect.outliers(data[n:(n+window.size),])
+# #       print(score)
+# #
+# #       n <- n + window.size
+# #       remaining.obs <- remaining.obs - window.size
+#        print(remaining.obs)
+#
+#     }
+
+  }
+
+}
+
+forecast.post <- function(data) {
+
+  ft <- forecast(as.ts(data))
+
+  return(ft$mean[2])
+
+}
+
+get.magnitude <- function(data) {
+
+  return(10^(ceiling(log10(data))))
+
+}
 
--- novelty_detection.R
+++ novelty_detection.R
@@ -10,59 +10,25 @@ library(jsonlite)
 library(httr)
 library(forecast)
 library(ggplot2)
+library(robfilter)
 
 
-#### Begin analysis ...
+### Begin analysis ...
 
-### Collect time series data that sufficiently exhibits the normal behavior of the system.
-## Data is standardized into z-score.
-series <- 'frblmci'
+## Collect time series data and standardize into z-score.
+series <- 'dgorder'
 object <- get.JSON(series)
 data <- get.data(object)
+
+## Collect vintages and metadata
 vintage <- get.vintage(series)
 metadata <- get.metadata(series)
 
-### Detect outliers (modify to detect outliers in windows).
-score <- detect.outliers(data$value)
+## Detect outliers by creating statistics that sufficiently describe the normal behavior of the system. This is process is completed over sliding windows of 10 observations.
+test <- observation.windows(data$value)
 
-### Create non-overlapping windows.
-## Count how many obs total, and handle obs remaining.
-series.length <- length(data$value)
-remaining.obs <- series.length
 
-## Establish number of windows over span of time series.
-## 10 is chosen to prevent errors surrounding degrees of freedom.
-window.size <- 10
-complete.windows <- floor(series.length/window.size)
-remainder <- series.length%%window.size
-slider <- 9
-
-## Counter variables for window coordinates in list (this is bad practice).
-n <- 1
-
-## Check if new data is identical to previous vintage up to the last observation.
-if (identical(tail(data$value, n=-1),vintage$value)) {
-
-  score <- detect.outliers(tail(data$value, n = 10)
-
-} else {
-
-  ## Sequence along windows of time series.
-  # Adjust to prevent subscript out of bounds.
-  for (i in seq_along(data$value)) {
-
-    print(i)
-
-    score <- detect.outliers(data$value[n:(n+window.size-1),])
-    print(score)
-
-    n <- n + slider
-    remaining.obs <- remaining.obs - window.size
-
-  }
-
-}
-
+## Forecast testing below
 # For tests: Visualize series
 # ggplot(data=data, aes(x=date,y=value)) +
 #   geom_line(colour="blue", size=.6) +
@@ -70,8 +36,7 @@ if (identical(tail(data$value, n=-1),vintage$value)) {
 
 
 ## Forecast testing
-# test.object <- fromJSON('http://api.stlouisfed.org/fred/series/observations?api_key=76bb1186e704598b725af0a27159fdfc//
-#                &file_type=json&units=lin&vintage_dates=2014-08-26&series_id=DGORDER')
+# test.object <- fromJSON('http://api.stlouisfed.org/fred/series/observations?api_key=76bb1186e704598b725af0a27159fdfc&file_type=json&units=lin&vintage_dates=2014-08-26&series_id=DGORDER')
 # test.data <- data.frame(test.object$observations)
 # View(test.data)
 # get.data(test.data)
@@ -84,4 +49,9 @@ if (identical(tail(data$value, n=-1),vintage$value)) {
 # View(test.data)
 # test.data <-head(test.data, n=-1)
 # test.score <- detect.outliers(test.data$value)
-# forecast(as.ts(test.data$value))
+# ft <- forecast(as.ts(test.data$value))
+# plot(ft)
+
+## Adaptive Robust Moving Median testing
+# test.filter <- adore.filter(as.ts(test.data$value),min.width = 5,max.width = 10,rtr=1)
+# plot(test.filter)
